8304d6e419894823905288f8f2417990e6e463d0
Prathmesh <94554810+Pratneo03@users.noreply.github.com>
2022-10-04 22:26:45 +0530

    4OCTLAST

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
+import data from './data';
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">BookWiser</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1> Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div class="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+//object to return a sample file
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt', //its what you see on url
+      cate: 'Shirts',
+      image: '/images/p1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+
+    {
+      name: 'Puma Slim shirt',
+      slug: 'puma-slim-shirt', //its what you see on url
+      cate: 'Shirts',
+      image: '/images/p2.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+
+    {
+      name: 'Adidas Slim shirt',
+      slug: 'adidas-slim-shirt', //its what you see on url
+      cate: 'Shirts',
+      image: '/images/p3.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+  ],
+};
+
+export default data;
