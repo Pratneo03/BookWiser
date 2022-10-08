ff81dfbccf84505b0262f2adc037c81e45038c5c
Prathmesh <94554810+Pratneo03@users.noreply.github.com>
2022-10-08 19:49:34 +0530

    2nd comit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,21 @@
-import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href="/">BookWiser</a>
-      </header>
-      <main>
-        <h1> Featured Products</h1>
-        <div className="products">
-          {data.products.map(
-            (
-              product //here product is the function
-            ) => (
-              <div className="product" key={product.slug}>
-                <a href={'/product/${product.slug}'}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={'/product/${product.slug}'}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            )
-          )}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header className="App-header">
+          <a href="/">BookWiser</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom';
+
+function ProductScreen() {
+  const params = useParams(); //We make use of hook called params here.
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>s
+    </div>
+  );
+}
+export default ProductScreen;
