67f20b2ae44389cb3f8f8d6e23cdf971fdf7de94
Prathmesh <94554810+Pratneo03@users.noreply.github.com>
2022-10-08 18:54:02 +0530

    Step1 done

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -2,42 +2,94 @@
 const data = {
   products: [
     {
-      name: 'Nike Slim shirt',
-      slug: 'nike-slim-shirt', //its what you see on url
-      cate: 'Shirts',
+      name: 'Book1',
+      slug: 'b1', //its what you see on url
+      cate: 'DS',
       image: '/images/img1.jfif',
-      price: 120,
+      price: 360,
       countInStock: 10,
       brand: 'nike',
       rating: 4.5,
       numReviews: 10,
-      description: 'high quality shirts',
+      description: 'Second hand books',
     },
 
     {
-      name: 'Puma Slim shirt',
-      slug: 'puma-slim-shirt', //its what you see on url
-      cate: 'Shirts',
+      name: 'Book2',
+      slug: 'b2', //its what you see on url
+      cate: 'Mech',
       image: '/images/img2.jfif',
       price: 120,
       countInStock: 10,
-      brand: 'puma',
+      brand: 'Manav publication',
       rating: 4.5,
       numReviews: 10,
-      description: 'high quality shirts',
+      description: 'Second hand books',
     },
 
     {
-      name: 'Adidas Slim shirt',
-      slug: 'adidas-slim-shirt', //its what you see on url
-      cate: 'Shirts',
+      name: 'Book3',
+      slug: 'b3', //its what you see on url
+      cate: 'CS',
       image: '/images/img3.jfif',
-      price: 120,
+      price: 890,
       countInStock: 10,
-      brand: 'adidas',
+      brand: 'Sharda Publication ',
       rating: 4.5,
       numReviews: 10,
-      description: 'high quality shirts',
+      description: 'Second hand books',
+    },
+
+    {
+      name: 'Book4',
+      slug: 'b4', //its what you see on url
+      cate: 'CG',
+      image: '/images/img4.jfif',
+      price: 890,
+      countInStock: 10,
+      brand: 'Sharda Publication',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Second hand books',
+    },
+
+    {
+      name: 'Book5',
+      slug: 'b5', //its what you see on url
+      cate: 'CE',
+      image: '/images/img5.jfif',
+      price: 890,
+      countInStock: 10,
+      brand: 'Anuj Publication',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Second hand books',
+    },
+
+    {
+      name: 'Book6',
+      slug: 'b6', //its what you see on url
+      cate: 'CE',
+      image: '/images/img6.jfif',
+      price: 890,
+      countInStock: 10,
+      brand: 'Anuj Publication',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Second hand books',
+    },
+
+    {
+      name: 'Book7',
+      slug: 'b7', //its what you see on url
+      cate: 'CE',
+      image: '/images/img7.jfif',
+      price: 890,
+      countInStock: 10,
+      brand: 'Anuj Publication',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Second hand books',
     },
   ],
 };
